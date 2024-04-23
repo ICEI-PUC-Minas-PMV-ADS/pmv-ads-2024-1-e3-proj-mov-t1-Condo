9c6b2815e78099e8a64e0f2f659bd21f6ae61504
Davi Haniel <davihani3l@gmail.com>
2024-04-23 20:57:22 -0300

    Ícone de meus espaços

--- src/Condo/navigation/Navigation.js
+++ src/Condo/navigation/Navigation.js
@@ -1,6 +1,7 @@
 // Navigation.js
 import React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
+import { IconButton } from 'react-native-paper';
 import { createStackNavigator } from '@react-navigation/stack';
 import ScreenNavigation from '../pages/ScreenNavigation/ScreenNavigation';
 import Home from '../pages/Home/Home';
@@ -20,6 +21,7 @@ import Instrucoes from '../pages/Instrucoes/Instrucoes';
 import CadastroDependente from '../pages/Dependentes/CadastroDependente';
 import MeusDependentes from '../pages/Dependentes/MeusDependentes';
 import Bloco001 from '../pages/ApartamentosCadastrados/Bloco001';
+import DrawerManutencoes from '../components/DrawerManutencoes';
 
 const Stack = createStackNavigator();
 
@@ -27,25 +29,92 @@ export default function Navigation() {
   return (
     <NavigationContainer>
       <Stack.Navigator initialRouteName="ScreenNavigation">
-        <Stack.Screen name='ScreenNavigation' component={ScreenNavigation}/>
-        <Stack.Screen name='Home' component={Home} />
-        <Stack.Screen name='Dependentes' component={Dependentes} options={{ title: 'Dependentes' }} />
-        <Stack.Screen name='Reservas' component={Reservas} options={{ title: 'Reservas' }}/>
-        <Stack.Screen name='ReservarEspaco' component={ReservarEspaco} options={{ title: 'Reservar Espaço' }} />
-        <Stack.Screen name='ReservarEspacoTwoScreen' component={ReservarEspacoTwoScrenn} options={{ title: 'Reservar Espaço' }}/>
-        <Stack.Screen name='LoginCondominio' component={LoginCondominio} options={{ title: 'Login Condomínio' }}/>
-        <Stack.Screen name='LoginMorador' component={LoginMorador} options={{ title: 'Login Condômino' }}/>
-        <Stack.Screen name='LoginMorador2' component={LoginMorador2} options={{ title: 'Login Condômino' }}/>
-        <Stack.Screen name='CadastroEspacos' component={CadastroEspacos}/>
-        <Stack.Screen name='CadastroApto' component={CadastroApto}/>
-        <Stack.Screen name='MeusEspacos' component={MeusEspacos}/>
-        <Stack.Screen name='ApartamentosCadastrados' component={ApartamentosCadastrados} options={{ title: 'Apartamentos Cadastrados' }}/>
-        <Stack.Screen name='Manutencao' component={Manutencao}/>
-        <Stack.Screen name='Instrucoes' component={Instrucoes}/>
-        <Stack.Screen name='CadastroDependente' component={CadastroDependente} options={{ title: 'Novo Dependente' }}/>
-        <Stack.Screen name='MeusDependentes' component={MeusDependentes} options={{ title: 'Meus Dependentes' }}/>
-        <Stack.Screen name='Bloco001' component={Bloco001} options={{ title: 'Bloco 001' }}/>
+        <Stack.Screen name="ScreenNavigation" component={ScreenNavigation} />
+        <Stack.Screen name="Home" component={Home} />
+        <Stack.Screen
+          name="Dependentes"
+          component={Dependentes}
+          options={{ title: 'Dependentes' }}
+        />
+        <Stack.Screen
+          name="Reservas"
+          component={Reservas}
+          options={{ title: 'Reservas' }}
+        />
+        <Stack.Screen
+          name="ReservarEspaco"
+          component={ReservarEspaco}
+          options={{ title: 'Reservar Espaço' }}
+        />
+        <Stack.Screen
+          name="ReservarEspacoTwoScreen"
+          component={ReservarEspacoTwoScrenn}
+          options={{ title: 'Reservar Espaço' }}
+        />
+        <Stack.Screen
+          name="LoginCondominio"
+          component={LoginCondominio}
+          options={{ title: 'Login Condomínio' }}
+        />
+        <Stack.Screen
+          name="LoginMorador"
+          component={LoginMorador}
+          options={{ title: 'Login Condômino' }}
+        />
+        <Stack.Screen
+          name="LoginMorador2"
+          component={LoginMorador2}
+          options={{ title: 'Login Condômino' }}
+        />
+        <Stack.Screen name="CadastroEspacos" component={CadastroEspacos} />
+        <Stack.Screen name="CadastroApto" component={CadastroApto} />
+        <Stack.Screen
+          name="MeusEspacos"
+          component={MeusEspacos}
+          options={() => ({
+            title: 'Meus Espaços',
+            headerRight: () => <SettingsIconManutencoes />,
+          })}
+        />
+        <Stack.Screen
+          name="ApartamentosCadastrados"
+          component={ApartamentosCadastrados}
+          options={{ title: 'Apartamentos Cadastrados' }}
+        />
+        <Stack.Screen name="Manutencao" component={Manutencao} />
+        <Stack.Screen name="Instrucoes" component={Instrucoes} />
+        <Stack.Screen
+          name="CadastroDependente"
+          component={CadastroDependente}
+          options={{ title: 'Novo Dependente' }}
+        />
+        <Stack.Screen
+          name="MeusDependentes"
+          component={MeusDependentes}
+          options={{ title: 'Meus Dependentes' }}
+        />
+        <Stack.Screen
+          name="Bloco001"
+          component={Bloco001}
+          options={{ title: 'Bloco 001' }}
+        />
       </Stack.Navigator>
     </NavigationContainer>
+  );
+}
+
+function SettingsIconManutencoes() {
+  const openDrawer = () => {
+
+  };
+
+  return (
+    <IconButton
+      icon='cog'
+      size={24}
+      color='black'
+      onPress={openDrawer}
+      style={{ marginRight: 10 }}
+    />
   );
 }
