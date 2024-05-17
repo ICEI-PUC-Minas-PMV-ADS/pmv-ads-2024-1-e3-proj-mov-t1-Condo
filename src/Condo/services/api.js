37ecc02454a3c1fe8f013de5710bd51f8de7f0fa
Josué Almeida <jbalmeida@sga.pucminas.Br>
2024-05-16 22:15:28 -0300

    Update 16/05

--- src/Condo/services/api.js
+++ src/Condo/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-    baseURL: "https://mock_610752d33a2c439aaaaaecda3ae9a7c5.mock.insomnia.rest"
+    baseURL: "https://condo.wiremockapi.cloud"
 })
 
 export default api;
--- src/Condo/services/urls.js
+++ src/Condo/services/urls.js
@@ -1,3 +1,1 @@
-import axios from "axios";
-
-export const baseURL = 'https://mock_610752d33a2c439aaaaaecda3ae9a7c5.mock.insomnia.rest';
+export const baseURL = 'https://condo.wiremockapi.cloud';
